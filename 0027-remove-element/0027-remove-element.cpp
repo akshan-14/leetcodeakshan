3d525b151dc0602928f3d704d3d108c19d32c32a
AKSHAN LAKHANPAL <iib2025003@iiita.ac.in>
2026-09-06 17:24:50 +0530

    Time: 0 ms (100%), Space: 11.8 MB (48.94%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        vector<int>ans;
+        for(auto x:nums){
+            if(x!=val){
+                ans.push_back(x);
+            }
+        }
+        nums=ans;
+        return ans.size();
+    }
+};
