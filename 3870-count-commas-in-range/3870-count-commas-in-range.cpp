7309f40003c7dfc0e0cf26d145ea9f21b7388bd6
AKSHAN LAKHANPAL <iib2025003@iiita.ac.in>
2026-09-08 19:34:58 +0530

    Time: 0 ms (100%), Space: 8.7 MB (7.49%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -5,12 +5,6 @@ public:
         if(dig<4){
             return 0;
         }
-        int ans=0;
-        while(dig>=4){
-            n--;
-            dig=log10(n)+1;
-            ans++;
-        }
-        return ans;
+        return (n-999);
     }
 };
