aed3cdd2d48e1d81c0712ab2da94834ec154fb2a
AKSHAN LAKHANPAL <iib2025003@iiita.ac.in>
2026-09-10 10:27:39 +0530

    Time: 1 ms (24.9%), Space: 21.8 MB (38.26%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int low=0;
+        int high=nums.size()-1;
+        while(low<=high){
+           int mid=(low+high)/2;
+           if(nums[mid]==mid){
+              low=mid+1;
+           }
+           else{
+            high=mid-1;
+           }
+        }
+        return low;
+    }
+};
